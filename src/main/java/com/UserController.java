a16065f5bb05b8d38e72a7308b67e49a7f654a10
Vitaliy <hladon@meta.ua>
2019-08-18 21:01:48 +0300

    1 commit

--- src/main/java/com/UserController.java
+++ src/main/java/com/UserController.java
@@ -3,11 +3,15 @@ package com;
 import com.models.User;
 import com.repository.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.ModelAndView;
 
 
 @Controller
@@ -22,13 +26,20 @@ public class UserController {
     }
 
     @RequestMapping(path = "/user/{userId}", method = RequestMethod.GET)
-    public String profile(Model model, @PathVariable String userId){
-        //TODO controller-service
-        long id=Long.parseLong(userId);
-        User user=userDao.findById(id);
-        model.addAttribute("user",user);
+    public String profile(Model model, @PathVariable long userId){
+        User user=null;
+        try {
+            user=userDao.findById(userId);
+            model.addAttribute("user",user);
+        }catch (Exception e){
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        if (user==null){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
         return "profile";
     }
+
 
 //    @RequestMapping(path = "/registration", method = RequestMethod.GET)
 //    public String registration(){
