8e4148adc652ceff0a1f04d35760c19b73081242
Vitaliy <hladon@meta.ua>
2019-10-03 17:44:02 +0300

    commit

--- src/main/java/com/PostService.java
+++ src/main/java/com/PostService.java
@@ -1,22 +1,48 @@
 package com;
 
+import com.models.Post;
 import com.models.User;
+import com.repository.PostDao;
+import com.repository.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 @Service
 public class PostService {
 
-    public ResponseEntity<String> addPost (String post, String url, User userClient){
+    @Autowired
+    private PostDao postDao;
+
+    private UserDao userDao;
+
+    public ResponseEntity<String> addPost (String message, String url, User userClient){
+        try {
 //        long pageId=Long.parseLong(url.substring(1));
-        System.out.println(url);
-        if(checkLinks(post))
-            return new ResponseEntity<>("Links not allowed here!",HttpStatus.BAD_REQUEST);
+            System.out.println(url);
+            if(checkLinks(message))
+                return new ResponseEntity<>("Links not allowed here!",HttpStatus.BAD_REQUEST);
+//            User userPage=userDao.findById(pageId);
+//            Post post=new Post();
+//            post.setDatePosted(new Date());
+//            post.setMessage(message);
+//            postDao.save(post);
+        }catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
         return new ResponseEntity<>(HttpStatus.ACCEPTED);
+    }
+
+    public List<Post> getPosts(String userIdPage){
+
+
     }
 
     private boolean checkLinks(String text) {
--- src/main/java/com/repository/PostDao.java
+++ src/main/java/com/repository/PostDao.java
@@ -1,9 +1,13 @@
 package com.repository;
 
 import com.models.Post;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class PostDao extends DAO<Post> {
     public PostDao() {
         type=Post.class;
     }
+
+
 }
