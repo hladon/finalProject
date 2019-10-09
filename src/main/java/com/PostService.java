93d78e83eb9afb7a761b945433da6f70d30bf080
Vitaliy <hladon@meta.ua>
2019-10-09 10:47:49 +0300

    commit

--- src/main/java/com/PostService.java
+++ src/main/java/com/PostService.java
@@ -10,7 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpServerErrorException;
+
 
 import javax.servlet.http.HttpSession;
 import java.util.Date;
