3918c7b5aca3f81688c9545f0d689e613c5c3838
Vitaliy <hladon@meta.ua>
2019-11-27 17:36:52 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/com/Exceptions/ViewsExceptionHandler.java
    #	src/main/java/com/controller/UserController.java
    #	src/main/java/com/service/MessageService.java

--- src/main/java/com/repository/MessageDAO.java
+++ src/main/java/com/repository/MessageDAO.java
@@ -4,6 +4,7 @@ import com.Exceptions.InternalServerError;
 import com.models.Message;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 
 @Repository
@@ -12,30 +13,42 @@ public class MessageDAO extends DAO<Message> {
         type = Message.class;
     }
 
-    private static final String GET_INCOME_MESSAGES = "SELECT * FROM MESSAGE WHERE DATE_DELETED IS NULL AND " +
-            "USER_TO=?1 AND ROWNUM<= 10 ORDER BY DATE_SEND ";
-    private static final String GET_OUTCOME_MESSAGES = "SELECT * FROM MESSAGE WHERE DATE_DELETED IS NULL AND " +
-            "USER_FROM=?1 AND ROWNUM<= 10 ORDER BY DATE_SEND ";
+    private static final String GET_MESSAGES = "SELECT * FROM MESSAGE WHERE DATE_DELETED IS NULL AND " +
+            " ?1 IN (USER_TO,USER_FROM) AND ?2 IN (USER_TO,USER_FROM)  DATE_SEND<?3 AND ROWNUM<= 20 ORDER BY DATE_SEND DESC ";
+    private static final String DELETE_ALL_MESSAGES = "UPDATE MESSAGE SET DATE_DELETED=?1 WHERE  ?2 IN (USER_TO,USER_FROM)";
+    private static final String DELETE_MESSAGE = "UPDATE MESSAGE SET DATE_DELETED=?1 WHERE  ID=?2";
 
-    public List<Message> getIncomeMessages(Long userId) throws InternalServerError {
+    public List<Message> getNextMessages(Long userId, Long userFromId, Date lastMessageDate) throws InternalServerError {
         try {
-            return entityManager.createNativeQuery(GET_INCOME_MESSAGES, Message.class)
+            return entityManager.createNativeQuery(GET_MESSAGES, Message.class)
                     .setParameter(1, userId)
+                    .setParameter(2, userFromId)
+                    .setParameter(3, lastMessageDate)
                     .getResultList();
         } catch (Exception e) {
             throw new InternalServerError(e.getMessage());
         }
-
     }
 
-    public List<Message> getOutcomeMessages(Long userId) throws InternalServerError {
+    public void deleteAllMessages(Long userFromId) throws InternalServerError {
         try {
-            return entityManager.createNativeQuery(GET_OUTCOME_MESSAGES, Message.class)
-                    .setParameter(1, userId)
+             entityManager.createNativeQuery(DELETE_ALL_MESSAGES, Message.class)
+                    .setParameter(1, new Date())
+                    .setParameter(2, userFromId)
                     .getResultList();
         } catch (Exception e) {
             throw new InternalServerError(e.getMessage());
         }
+    }
 
+    public void deleteMessage(Long messageId,Date dateDeleted) throws InternalServerError {
+        try {
+            entityManager.createNativeQuery(DELETE_MESSAGE, Message.class)
+                    .setParameter(1, dateDeleted)
+                    .setParameter(2, messageId)
+                    .getResultList();
+        } catch (Exception e) {
+            throw new InternalServerError(e.getMessage());
+        }
     }
 }
