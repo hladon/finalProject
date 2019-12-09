d615c8f3692e2fdb39b8fb65f0f8d30a776850f2
Vitaliy <hladon@meta.ua>
2019-12-09 18:06:27 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/com/Exceptions/ViewsExceptionHandler.java
    #	src/main/java/com/controller/UserController.java
    #	src/main/java/com/service/MessageService.java

---
+++ src/main/java/com/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.sql.DataSource;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .authoritiesByUsernameQuery("SELECT u.phone, r.role FROM user INNER JOIN USER_ROLE ur ON " +
+                        "u.id=ur.userId where u.phone=?")
+                .dataSource(dataSource);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("")
+    }
+}
